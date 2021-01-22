80de2fcdada690d9b0218e2c4a56b1e98085e6fa
Liydmyla Chubko <59334344+Chubko@users.noreply.github.com>
2021-01-22 15:40:34 +0200

    Delete react-practise-homework directory

--- react-practise-homework/src/services/PostService.js
+++
@@ -1,8 +0,0 @@
-export default class PostService{
-    url = 'https://jsonplaceholder.typicode.com/posts?userId=';
-
-    async getPost(id){
-        return await fetch(this.url + id)
-            .then(value => value.json())
-    }
-}
