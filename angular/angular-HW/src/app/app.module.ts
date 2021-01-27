710c5be05f27b3ead36d3d9ada4db227f3901b1a
Liydmyla Chubko <luda.chubko@gmail.com>
2021-01-27 16:09:41 +0200

    homework4

--- angular/angular-HW/src/app/app.module.ts
+++ angular/angular-HW/src/app/app.module.ts
@@ -2,13 +2,46 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { UsersComponent } from './components/users/users.component';
+import {RouterModule, Routes} from '@angular/router';
+import {UserResolveService} from './services/resolve/user-resolve.service';
+import { UserComponent } from './components/user/user.component';
+import { FullUserComponent } from './components/full-user/full-user.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostComponent } from './components/post/post.component';
+import {PostResolveService} from './services/resolve/post-resolve.service';
 
+
+const routes: Routes = [
+  {
+    path: 'users', component: UsersComponent, resolve: {usersData: UserResolveService},
+    children: [
+      {
+        path: ':id', component: FullUserComponent,
+        children: [
+          {path: 'posts', component: PostsComponent, resolve: {postsData: PostResolveService},
+          children: [
+            {path: ':id', component: PostComponent}
+          ]}
+        ]}
+    ]
+  },
+  {path: '', redirectTo: 'users', pathMatch: 'full'}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    FullUserComponent,
+    PostsComponent,
+    PostComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
