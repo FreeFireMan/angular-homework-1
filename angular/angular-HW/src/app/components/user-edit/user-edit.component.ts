ff8312484f29b5aa4803d39531bba489453cc220
Liydmyla Chubko <luda.chubko@gmail.com>
2021-01-29 15:39:52 +0200

    homework5 Forms

--- angular/angular-HW/src/app/components/user-edit/user-edit.component.ts
+++ angular/angular-HW/src/app/components/user-edit/user-edit.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, NgForm} from '@angular/forms';
+import {FormControl, FormGroup, NgForm, Validators} from '@angular/forms';
 import {User} from '../../interfaces/user';
 import {ActivatedRoute, Router} from '@angular/router';
 
@@ -10,6 +10,10 @@ import {ActivatedRoute, Router} from '@angular/router';
 })
 export class UserEditComponent implements OnInit {
   user: User;
+  form: FormGroup;
+  name: any;
+  age: any;
+  email: any;
 
   constructor(private router: Router, private activatedRoute: ActivatedRoute) {
     this.activatedRoute.params.subscribe(value => {
@@ -17,18 +21,19 @@ export class UserEditComponent implements OnInit {
     });
   }
 
-  name = new FormControl(this.user.name);
-  age = new FormControl(this.user.age);
-  email = new FormControl(this.user.email);
-  form = new FormGroup({
-    name: this.name,
-    age: this.age,
-    email: this.email
-  });
-
   ngOnInit(): void {
+    this.name = new FormControl(this.user.name, [Validators.required]);
+    this.age = new FormControl(this.user.age, [Validators.required, Validators.min(0), Validators.max(100)]);
+    this.email = new FormControl(this.user.email, [Validators.required, Validators.email]);
+    this.form = new FormGroup({
+      name: this.name,
+      age: this.age,
+      email: this.email
+    });
     console.log(this.user);
   }
+
+
 
   // submitChanges(form: NgForm): void {
   //   console.log(form);
